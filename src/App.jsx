ee89f79fe11daea99fbb2de8762ac09191c72754
tasib94 <rahi84974@gmail.com>
2025-05-06 01:19:55 +0600

    practice project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-5xl text-lime-300 font-extrabold'>Email password authentication</h1>
+     <button className='text-amber-400 bg-amber-500'>test</button>
+      
+    </>
+  )
+}
+
+export default App
